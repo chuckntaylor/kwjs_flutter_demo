bf3f07ed8c63330d502f4d1450cfc3647030be34
Chuck Taylor <chuckntaylor@gmail.com>
2020-05-25 16:27:44 -0400

    Layout updates for the Events screen

--- lib/ui/views/events_screen.dart
+++ lib/ui/views/events_screen.dart
@@ -1,3 +1,4 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:kwjsflutter/business_logic/view_models/events_screen_viewmodel.dart';
 import 'package:kwjsflutter/services/service_locator.dart';
@@ -13,6 +14,22 @@ class EventsScreen extends StatefulWidget {
 class _EventsScreenState extends State<EventsScreen> {
 
   EventsScreenViewModel model = serviceLocator<EventsScreenViewModel>();
+
+  // Styles
+  final TextStyle dateStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 20
+  );
+
+  final TextStyle titleStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 16
+  );
+
+  final TextStyle attendeesStyle = TextStyle(
+    color: Colors.grey,
+    fontSize: 14
+  );
 
 
   @override
@@ -40,12 +57,42 @@ class _EventsScreenState extends State<EventsScreen> {
             itemCount: model.events.length,
             itemBuilder: (context, index) {
               return Card(
-                child: ListTile(
-                  title: Text('${model.events[index].name}'),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IntrinsicHeight(
+                    child: Row(
+                      children: [
+                        Container(
+                            child: Text(_formatDate(model.events[index].startsAt),
+                            style: dateStyle,)
+                        ),
+                        VerticalDivider(
+                          width: 30,
+                          thickness: 1,
+                          color: Colors.grey
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('${model.events[index].name}', style: titleStyle,),
+                              SizedBox(height: 8,),
+                              Text('${model.events[index].attendeesCount} attendees', style: attendeesStyle,)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               );
             }),
       ),
     );
+  }
+
+  String _formatDate(String date) {
+    final _parsedDate = DateTime.parse(date);
+    return formatDate(_parsedDate, ['M', ' ', 'd']);
   }
 }
